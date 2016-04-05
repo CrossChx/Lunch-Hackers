f97a81ad7693aa745e3ead2865d0c1d7fa2e0dde
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-05 14:32:10 -0400

    redundant, I suppose

--- hackerrank_keyword-transposition-cipher/paulhendricks/cypher.py
+++ hackerrank_keyword-transposition-cipher/paulhendricks/cypher.py
@@ -1,6 +1,4 @@
 #!/bin/python3
-import numpy as np
-import string
 
 
 def fix_keyword(x):
@@ -14,17 +12,18 @@ def fix_keyword(x):
 
 
 def translate(raw_keyword, message):
-    alphabet = string.ascii_uppercase
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     keyword = fix_keyword(raw_keyword)
     n = len(keyword)
-    keyword_sorted = np.unique(np.sort([i for i in keyword]))
-    possible = np.array([i for i in alphabet if i not in keyword_sorted])
-    arr = np.r_[keyword, possible]
-    arr2 = [arr[i::n] for i in np.arange(n)]
+    keyword_sorted = keyword.copy()
+    keyword_sorted.sort()
+    possible = [i for i in alphabet if i not in keyword_sorted]
+    arr = keyword + possible
+    arr2 = [arr[i::n] for i in range(n)]
     arr3 = {i: j for i, j in zip(keyword, arr2)}
     cipher_alphabet = []
     for i in keyword_sorted:
-        cipher_alphabet = np.r_[cipher_alphabet, arr3[i]]
+        cipher_alphabet = cipher_alphabet + arr3[i]
     cipher = {c: l for c, l, in zip(cipher_alphabet, alphabet)}
     solved = []
     for s in message:
@@ -37,13 +36,9 @@ def translate(raw_keyword, message):
 
 
 def main():
-    file_name = '../input.txt'
-    with open(file_name, 'r') as f:
-        x = f.read().strip().split('\n')
-    t = int(x[0].strip())
-    remainder = x[1:]
-    for i in np.arange(0, t + 1, 2):
-        print(translate(remainder[i], remainder[i + 1]))
+    t = int(input())
+    for i in range(t):
+        print(translate(input(), input()))
 
 
 if __name__ == '__main__':
